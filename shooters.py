be649c2a95d7b3cd71d3dedaeb4c01f0ea640b37
edunham <dunhame@onid.orst.edu>
2013-04-30 12:37:30 -0700

    be stupidly explicit about units

--- shooters.py
+++ shooters.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python
 
 class shooter:
-    def __init__(self, offset, xpos):
-        self.offset = offset # :t offset Inches
-        self.xpos = xpos
+    def __init__(self, offset, xpos, dpi):
+        self.offsetin = offset
+        self.offsetpx = offset * dpi
+        self.xposin = xpos
+        self.xpospx = xpos * dpi
         self.aim = 128
     def can_hit(target):
         return True
 
-LeftShooter = shooter(3, 8)
-CenterSooter = shooter(3, 12) 
-RightShooter = shooter(3, 16)
+LeftShooter = shooter(3, 8, 17)
+CenterSooter = shooter(3, 12, 17) 
+RightShooter = shooter(3, 16, 17)
 
