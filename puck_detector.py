0dbb2e495d08bd1246581c8fa5e285409afd4905
edunham <dunhame@onid.orst.edu>
2013-04-30 12:34:45 -0700

    rip logic out of the puck detector

--- puck_detector.py
+++ puck_detector.py
@@ -10,25 +10,18 @@ import cv2
 import time
 import numpy
 import os
-from shooters import *
 
 def open_camera(cam_id = 0):
     cap = cv2.VideoCapture(cam_id)
     cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 600);
     cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 800);
     cap.set(cv2.cv.CV_CAP_PROP_FPS, 30);
-hooter = shooter(3, 8) 
-CenterSooter = shooter(3, 12) 
-RightShooter = shooter(3, 16)
-
-
 
 def get_frame(device):
     ret, img = device.read()
     if (ret == False): # failed to capture
         print >> sys.stderr, "Error capturing from video device."
         return None
-
     return img
 
 def cleanup(cam_id = 0):
@@ -113,91 +106,3 @@ being calc'd in inches for some damn reason. Divide by dpi for trigs.
 def new_rgb_image(width, height):
     image = numpy.zeros( (height, width, 3), numpy.uint8)
     return image
-
-def choose_center(centers):
-    nearesty = sorted(centers, key = lambda pair: pair[0])
-    # blindspot logic goes here
-    return nearesty[0]
-
-def aim_shooter(centers, shooter):
-    deg = 45
-    target = choose_center(centers) # :t target (x,y) 
-    
-
-    if deg == 0:
-        aim = 1
-    else:
-        aim = (17.0/6) * deg
-    return aim
-
-def draw_aim(aim, image):
-    #for center in centers:
-    #    cv2.circle(image, tuple(center), 20, cv2.cv.RGB(0,255,255), 2)
-    pass
-
-
-if __name__ == "__main__":
-    cam_id=0
-    dev = open_camera(cam_id)
-
-    board_size = [22.3125, 45] # dimensions in inches of the part of the board
-        # we care about -- measured robot line to robot line
-    dpi = 17 # for all operations after skewing the image
-
-    do_calibration = True
-    calib_file = None
-
-    if (len(sys.argv) > 1):
-        calib_file = sys.argv[1]
-        if os.path.exists(calib_file):
-            do_calibration = False
-        else:
-            do_calibration = True
-
-    if do_calibration:
-        print "CALIBRATING:"
-        print "top left, bottom left, top right, bottom right."
-        transform = get_transform_matrix(dev, board_size, dpi, calib_file)
-    else: # load calibration from file
-        transform = numpy.loadtxt(calib_file)
-
-    print transform
-
-    transform_size = (int(board_size[0]*dpi), int(board_size[1]*dpi))
-    def trackbar_fnc(val):
-        global thresh_val
-        thresh_val = val
-    thresh_val = 220
-    cv2.createTrackbar('threshhold', "thresh", thresh_val, 255, trackbar_fnc)
-    while True:
-        img_orig = get_frame(dev)
-        if img_orig is not None: # if we did get an image
-            cv2.imshow("video", img_orig)
-            img = cv2.warpPerspective(img_orig, transform, dsize=transform_size)
-            # dsize is for calculations; hack things before the imshows if
-            # necessary
-            cv2.imshow("warped", img)
-            
-            img_gray = rgb_to_gray(img)
-            (im_thresh, im_bw) = do_threshold(img_gray, thresh_val)
-            cv2.imshow("thresh", im_bw)
-
-            contours = find_contours(im_bw)
-            centers = find_centers(contours)
-
-            aim = aim_shooter(centers, CenterShooter)
-
-            results_image = new_rgb_image(transform_size[0], transform_size[1])
-            cv2.drawContours(results_image, contours, -1, cv2.cv.RGB(255,0,0), 2)
-
-            draw_aim(aim, results_image)
-            draw_centers(centers, results_image)
-            cv2.imshow("results", results_image)
-
-        else: # if we failed to capture (camera disconnected?), then quit
-            break
-
-        if (cv2.waitKey(2) >= 0):
-            break
-
-    cleanup(cam_id)
