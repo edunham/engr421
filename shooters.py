28ce2038dad3b4d5806c9f2a051ee85d89ecb32c
edunham <dunhame@onid.orst.edu>
2013-05-01 13:44:48 -0700

    actually shooters should aim themselves, since it varies based on individual position

--- shooters.py
+++ shooters.py
@@ -1,16 +1,43 @@
 #! /usr/bin/env python
+from arduino.py import Ardiuno
 
 class shooter:
-    def __init__(self, offset, xpos, dpi):
+    def __init__(self, offset, xpos, dpi, comms, n):
         self.offsetin = offset
         self.offsetpx = offset * dpi
         self.xposin = xpos
         self.xpospx = xpos * dpi
+
+        self.leftdeg = 0
+        self.centerdeg = 45
+        self.rightdeg = 90
+
         self.aim = 128
-    def can_hit(target):
+        self.shot = 0
+        self.comms = comms
+        self.number = n
+
+    def can_hit(self, target):
         return True
 
-LeftShooter = shooter(3, 8, 17)
-CenterSooter = shooter(3, 12, 17) 
-RightShooter = shooter(3, 16, 17)
+    def target2angle(target):
+        opp = target[0] - self.xpospx
+        adj = target[1] + self.offsetpx
+        deg = self.centerdeg + math.degrees(math.atan(opp/adj))
+        if deg <= self.leftdeg:
+            return 1
+        elif deg >= self.rightdeg:
+            return 128
+        else:
+            return (17.0/6) * deg
+
+    def aim(self, target):
+        if self.can_hit(target):
+            self.aim = target2angle(target)
+            self.comms.aim(self.number, self.aim)
+
+Board = Arduino()
+LeftShooter = shooter(3, 8, 17, Board, 0)
+CenterSooter = shooter(3, 12, 17, Board, 1) 
+RightShooter = shooter(3, 16, 17, Board, 2)
 
