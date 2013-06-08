5200fdf6f94b2098a68545a396ca2c9456cb4903
edunham <dunhame@onid.orst.edu>
2013-06-07 19:48:08 -0700

    make it work with SerialCommander program

--- arduino.py
+++ arduino.py
@@ -2,6 +2,57 @@
 import serial
 import os
 
+class SerialCommander:
+    def __init__(self):
+        self.baudrate = 9600
+        if os.path.exists('/dev/ttyACM0'):
+            devpath = '/dev/ttyACM0'
+        elif os.path.exists('/dev/ttyACM1'):
+            devpath = '/dev/ttyACM1'
+        else:
+            print "dude, plug in the Arduino"
+            exit()
+        self.ser = serial.Serial(port=devpath, baudrate=self.baudrate, timeout=0)
+        self.ser.writeTimeout = 0
+        self.comms = {"aim": '\x01',
+                      "fire": '\x02',
+                      "GO": 'GO'}
+        self.infos = {"start": '\x80',
+                      "bad command": '\xE0',
+                      "bad shooter": '\xE1',
+                      "bad angle": '\xE2'}
+        self.shooters = {"left": '\x01',
+                         "center": '\x02',
+                         "right": '\x03',
+                         "error": '\x13'}
+        self.read()
+
+    def send(self, data):
+        self.ser.write(data)
+
+    def read(self):
+        prefix = "\tREAD:"
+        while self.ser.inWaiting() > 0: # something there to read
+            line = self.ser.readline()[:-1] # strip \n
+            for msg, val in self.infos.iteritems():
+                if val in line:
+                    handle_message(msg, line)
+            print prefix + line
+            prefix = "\t\t"
+        self.ser.flushInput() # else eventual freeze
+        self.ser.flushOutput()
+
+    def aim(self, shooter, angle):
+        data = self.shooters[shooter] + ' ' + angle
+        self.send(data)
+        self.read()
+
+    def fire(self, shooter):
+        data = self.shooters[shooter] + ' s'
+        self.send(data)
+        self.read()
+
+
 class Arduino:
     def __init__(self):
         self.baudrate = 9600
